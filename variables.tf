0399efa14658a96cf0011e2e15eb9b0041563878
GersonRS <gersonrodriguessantos8@gmail.com>
2026-07-11 18:32:53 -0300

    feat: add namespace_labels variable to all modules

--- variables.tf
+++ variables.tf
@@ -81,6 +81,14 @@ variable "dependency_ids" {
   default     = {}
 }
 
+variable "namespace_labels" {
+  description = "Labels to apply to the destination namespace managed by Argo CD (requires CreateNamespace=true)."
+  type        = map(string)
+  default = {
+    "istio.io/dataplane-mode" = "ambient"
+  }
+}
+
 variable "kafka_name" {
   description = "Name of the kafka broker"
   type        = string
